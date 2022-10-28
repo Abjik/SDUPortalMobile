f3ae57a3691e2962bf79f609b9d26c94a6e3a070
Abjik <200107009@stu.sdu.edu.kz>
2022-10-29 05:06:18 +0600

    Initial commit

---
+++ app/src/main/java/com/example/sdu_portal_mobile/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sdu_portal_mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.todo_list)
+    }
+}
