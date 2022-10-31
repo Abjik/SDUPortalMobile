d0dbfb3c04825efeef98853c62bd1fd4a4cba20f
numpadCrusader <200107044@stu.sdu.edu.kz>
2022-10-31 20:26:47 +0600

    fixes

--- app/src/main/java/com/example/sdu_portal_mobile/model/TodoModel.kt
+++ app/src/main/java/com/example/sdu_portal_mobile/model/TodoModel.kt
@@ -1,4 +1,6 @@
 package com.example.sdu_portal_mobile.model
 
 data class TodoModel(
-    val todoTitle:String, val todoSubtitle:String)
+    val todo_Title:String,
+    val todo_Subtitle:String
+    )
