7712d0c443b470ed55b3060db5a345cb12a183d9
Abjik <200107009@stu.sdu.edu.kz>
2022-10-31 17:46:44 +0600

    test login and password

--- app/src/main/java/com/example/sdu_portal_mobile/MainActivity.kt
+++ app/src/main/java/com/example/sdu_portal_mobile/MainActivity.kt
@@ -2,12 +2,37 @@ package com.example.sdu_portal_mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.sdu_portal_mobile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var binding = ActivityMainBinding.inflate(layoutInflater)
+        binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        navController= Navigation.findNavController(this,R.id.activity_main_nav_host_fragment)
+        setupWithNavController(binding.bottomNavigationView,navController)
+
+    }
+
+    var user = findViewById<EditText>(R.id.login)
+    var password = findViewById<EditText>(R.id.password)
+
+    fun login(){
+        if(user.text.toString().equals("admin") && password.text.toString().equals("admin")){
+
+        }
+        else{
+
+        }
     }
 }
