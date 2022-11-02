a1a6361f6feafaae013707db6e646986dbd5eaec
numpadCrusader <200107044@stu.sdu.edu.kz>
2022-11-02 13:25:54 +0600

    todos now work & fixes

--- app/src/main/java/com/example/sdu_portal_mobile/TodosFragment.kt
+++ app/src/main/java/com/example/sdu_portal_mobile/TodosFragment.kt
@@ -1,23 +1,23 @@
 package com.example.sdu_portal_mobile
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import com.example.sdu_portal_mobile.databinding.FragmentAddTodoBinding
+import com.example.sdu_portal_mobile.databinding.FragmentTodosBinding
 
-class AddTodoFragment : Fragment() {
+class TodosFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = FragmentAddTodoBinding.inflate(inflater)
+        val binding = FragmentTodosBinding.inflate(inflater)
 
-        binding.addTodo.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_addTodoFragment_to_todosFragment)
+        binding.floatingActionButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_todosFragment_to_addTodoFragment)
         }
         return binding.root
     }
