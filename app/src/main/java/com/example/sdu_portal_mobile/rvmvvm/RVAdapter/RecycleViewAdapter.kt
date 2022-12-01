ea52d94397dea73383833d0b5f18b2ddac099dac
numpadCrusader <200107044@stu.sdu.edu.kz>
2022-12-01 13:30:04 +0600

    fix list_adpater

--- app/src/main/java/com/example/sdu_portal_mobile/rvmvvm/RVAdapter/RecycleViewAdapter.kt
+++ app/src/main/java/com/example/sdu_portal_mobile/rvmvvm/RVAdapter/RecycleViewAdapter.kt
@@ -3,13 +3,15 @@ package com.example.sdu_portal_mobile.rvmvvm.RVAdapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sdu_portal_mobile.R
 import com.example.sdu_portal_mobile.databinding.ListItemBinding
 import com.example.sdu_portal_mobile.rvmvvm.Task
 
 class RecycleViewAdapter(private val clickListener: (Task) -> Unit) :
-    RecyclerView.Adapter<MyViewHolder>() {
+    ListAdapter<Task, MyViewHolder>(TaskDiffCallback) {
     private val taskList = ArrayList<Task>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -33,6 +35,16 @@ class RecycleViewAdapter(private val clickListener: (Task) -> Unit) :
 
     }
 
+}
+
+object TaskDiffCallback : DiffUtil.ItemCallback<Task>() {
+    override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+        return oldItem == newItem
+    }
 }
 
 class MyViewHolder(val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
